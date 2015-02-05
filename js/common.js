4b7fed639c6e8b105b7c37eb8aa4f2753e276cd4
tianxudong <tianxudong@tp-link.net>
2015-02-05 17:52:47 +0800

    change bgs

--- js/common.js
+++ js/common.js
@@ -24,11 +24,11 @@ window.onload = function() {
         if ($(this).hasClass('close')) {
             $('#bgm')[0].play();
             $(this).addClass('play').removeClass('close');
-            $(this).find('p').text('开启').addClass('fadeIn');
+            $(this).find('p').text('music on').addClass('fadeIn');
         } else if ($(this).hasClass('play')) {
             $('#bgm')[0].pause();
             $(this).removeClass('play').addClass('close');
-            $(this).find('p').text('关闭').addClass('fadeIn');
+            $(this).find('p').text('music off').addClass('fadeIn');
         }
         setTimeout(function() {
             $('.music-control').find('p').removeClass('fadeIn');
